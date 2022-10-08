dbb187b2fdd5c3ef3db60f06db79fd58bd92f32c
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-08 16:09:51 +0530

    Update problem3.js

--- problem3.js
+++ problem3.js
@@ -3,7 +3,16 @@ let problem3 = (inventory) => {
     for (let i=0;i<inventory.length;i++) {
         cars.push(inventory[i].car_model)
     }
-    return cars
+    cars.sort()
+    let sortedInventory = []
+    for (let car_name of cars) {
+        for (let car of inventory) {
+            if (car.car_model === car_name) {
+                sortedInventory.push(car)
+            }
+        }
+    }
+    return sortedInventory
 }
 
 module.exports = problem3
