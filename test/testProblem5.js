9abd18632f1eff1ac67b045437a6be8bec7a1f23
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-17 20:54:00 +0530

    Code is modified in this file.

--- test/testProblem5.js
+++ test/testProblem5.js
@@ -1,6 +1,6 @@
 const problem5 = require('../problem5')
 const cars_years = require("./testProblem4")
 
-let cars_made_before_2000 = problem5(cars_years)
+const cars_made_before_2000 = problem5(cars_years)
 console.log(cars_made_before_2000)
 console.log(cars_made_before_2000.length)
