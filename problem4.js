8a5066ea9a907c4ac3a2ad2acaeba4dcd103728e
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-17 20:09:12 +0530

    Code is modified in this file.

--- problem4.js
+++ problem4.js
@@ -1,5 +1,5 @@
-let problem4 = (inventory) => {
-    let cars = []
+const problem4 = (inventory) => {
+    const cars = []
     for (let i=0;i<inventory.length;i++) {
         cars.push(inventory[i].car_year)
     }
