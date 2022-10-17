841b898b90f6d07b6795087befe7123ede38dfb5
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-17 20:11:07 +0530

    Code is modified in this file.

--- problem5.js
+++ problem5.js
@@ -1,6 +1,5 @@
-const problem5 = (cars) => {
-    let year = 2000
-    let cars_made_before_2000 = []
+const problem5 = (cars, year=2000) => {
+    const cars_made_before_2000 = []
     for (let car_year of cars) {
         if (car_year < year) {
             cars_made_before_2000.push(car_year)
