fc5a553d21682f0ca642d7eb5094a3c36b5fe12b
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-17 20:49:17 +0530

    Code is modified in this file.

--- test/testProblem1.js
+++ test/testProblem1.js
@@ -1,7 +1,7 @@
 const inventory = require("../inventory.js")
 const problem1 = require("../problem1.js")
 
-const car = problem1(inventory)
+const resultFromProblem1 = problem1(inventory)
 
-let {car_year, car_make,car_model} = car
+const {car_year, car_make,car_model} = resultFromProblem1
 console.log(`Car 33 is a ${car_year} ${car_make} ${car_model}`)
