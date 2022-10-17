dfaf3e41b05c170eec512080a283af6c9f6bfe70
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-17 20:51:54 +0530

    Code is modified in this file.

--- test/testProblem3.js
+++ test/testProblem3.js
@@ -1,6 +1,6 @@
 const inventory = require("../inventory.js")
 const problem3 = require("../problem3.js")
 
-const result3 = problem3(inventory)
+const resultOfProblem3 = problem3(inventory)
 
-console.log(result3)
+console.log(resultOfProblem3)
